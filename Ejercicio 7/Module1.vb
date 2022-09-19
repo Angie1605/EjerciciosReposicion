ebd52561eb101fcbcce1e61d5cb4751ec7fe2680
Angie1605 <80908543+Angie1605@users.noreply.github.com>
2022-09-19 00:55:08 -0500

    Add files via upload

---
+++ Ejercicio 7/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim numero As Integer
+        Dim raizDigital As Integer
+
+        Console.WriteLine("Ingrese un numero entero")
+        numero = Console.ReadLine
+
+        While numero > 0
+            raizDigital += (numero Mod 10)
+            numero /= 10
+        End While
+
+        Console.WriteLine("La raiz digital del numero es: {0}", raizDigital)
+
+    End Sub
+
+End Module
