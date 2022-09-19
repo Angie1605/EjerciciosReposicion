2c322f0deba6fadb5e3c7dc97ee6eddf813b453f
Angie1605 <80908543+Angie1605@users.noreply.github.com>
2022-09-18 23:44:50 -0500

    Add files via upload

---
+++ Ejercicio 3/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim num1 As Integer
+
+
+        Console.WriteLine("Ingrese el primer numero")
+        num1 = Console.ReadLine
+        While num1 <> 1
+            If num1 Mod 2 = 0 Then
+                num1 /= 2
+            ElseIf num1 Mod 2 <> 0 Then
+                num1 = (num1 * 3) + 1
+
+            End If
+
+            Console.WriteLine(num1)
+
+        End While
+
+    End Sub
+
+End Module
