a6852e848fda56262e35a2abb969000a44495434
Angie1605 <80908543+Angie1605@users.noreply.github.com>
2022-09-18 23:23:08 -0500

    Add files via upload

---
+++ Ejercicio 1/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim num1 As Integer
+        Dim num2 As Integer
+
+        Console.WriteLine("Escriba el valor de X")
+        num1 = Console.ReadLine()
+        Console.WriteLine("Escriba el valor de Y")
+        num2 = Console.ReadLine()
+        Console.WriteLine("La potencia es: ")
+        Console.WriteLine(potencia(num1, num2))
+
+
+
+    End Sub
+    Public Function potencia(num1 As Integer, num2 As Integer) As Integer
+
+        Return (num1 ^ num2)
+
+    End Function
+End Module
