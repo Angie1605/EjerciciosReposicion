2def924352154f6d3a334d64748bcb114c4ffe93
Angie1605 <80908543+Angie1605@users.noreply.github.com>
2022-09-18 23:23:44 -0500

    Add files via upload

---
+++ Ejercicio 2/Module1.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim num1 As Integer
+        Dim num2 As Integer
+        Dim suma As Integer
+        Dim i As Integer
+
+        Console.WriteLine("Ingrese el primer numero")
+        num1 = Console.ReadLine
+        Console.WriteLine("Ingrese el segundo numero")
+        num2 = Console.ReadLine
+        i = num1
+
+        If num1 < num2 Then
+            Do
+                suma = num1 + suma
+                num1 += 1
+                i += 1
+                Console.WriteLine("La suma del intervalo es", suma)
+            Loop Until (i > num2)
+        Else
+            Console.WriteLine("No se puede continuar")
+
+        End If
+
+
+    End Sub
+
+End Module
