48111595779db47d10da4f92438fac4718c15431
Angie1605 <80908543+Angie1605@users.noreply.github.com>
2022-09-18 23:59:10 -0500

    Add files via upload

---
+++ Ejercicio 4/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim num As Integer
+        Dim i = 1
+        Dim Factorial = 1
+
+        Console.WriteLine("Ingrese un numero entero")
+        num = Console.ReadLine
+
+        While i <= num
+            Factorial *= i
+            i += 1
+        End While
+        Console.WriteLine("El factorial del numero {0} es: {1}", num, Factorial)
+
+    End Sub
+
+End Module
