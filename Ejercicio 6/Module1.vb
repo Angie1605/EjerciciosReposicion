4b0883bb773c59adc80a0a0116adfebc8c9cc985
Angie1605 <80908543+Angie1605@users.noreply.github.com>
2022-09-19 00:40:05 -0500

    Add files via upload

---
+++ Ejercicio 6/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim numero As Integer
+
+        Console.WriteLine("Ingrese un numero")
+        numero = Console.ReadLine
+        Console.WriteLine("Piramide")
+        For i As Integer = 1 To numero Step 1
+
+            For j As Integer = 1 To i Step 1
+
+                Console.Write(i)
+            Next
+            Console.WriteLine("  ")
+        Next
+
+
+
+
+    End Sub
+
+End Module
