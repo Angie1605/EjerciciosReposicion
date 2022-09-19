239e228f628c5b573e0e29826d026edb4be8fa3b
Angie1605 <80908543+Angie1605@users.noreply.github.com>
2022-09-19 01:15:08 -0500

    Add files via upload

--- Ejercicio 7/Module1.vb
+++ Ejercicio 7/Module1.vb
@@ -2,17 +2,28 @@
 
     Sub Main()
         Dim numero As Integer
-        Dim raizDigital As Integer
+        Dim suma = 0
+        Dim raiz As Integer
+
 
         Console.WriteLine("Ingrese un numero entero")
         numero = Console.ReadLine
 
         While numero > 0
-            raizDigital += (numero Mod 10)
+            Dim digito As Integer = (numero Mod 10)
             numero /= 10
+            suma += digito
+            If suma > 9 Then
+                While suma > 0
+                    raiz += (suma Mod 10)
+                    suma /= 10
+                End While
+                suma = raiz
+            End If
         End While
 
-        Console.WriteLine("La raiz digital del numero es: {0}", raizDigital)
+
+        Console.WriteLine("La raiz digital del numero es: {0}", suma)
 
     End Sub
 
