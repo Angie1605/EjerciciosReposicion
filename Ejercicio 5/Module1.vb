00521b074e8dcfb083a7403ee72b7131189a136d
Angie1605 <80908543+Angie1605@users.noreply.github.com>
2022-09-19 00:13:02 -0500

    Add files via upload

---
+++ Ejercicio 5/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim numero As String
+        Dim digito As Integer
+
+        Console.WriteLine("Ingrese un numero entero")
+        numero = Console.ReadLine
+        Console.WriteLine("La catidad de digitos del numero es:")
+        Console.WriteLine(numero.Length)
+
+
+
+
+    End Sub
+
+End Module
